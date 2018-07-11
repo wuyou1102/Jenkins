f56ba69448faefb8117846a81ba665f0fc280788
wuyou1102 <jotey@qq.com>
2018-07-11 11:20:31 +0800

    add

---
+++ src/Pipeline.groovy
@@ -0,0 +1,35 @@
+import com.wuyou.Learn
+
+
+node {
+    // Clean workspace before doing anything
+    deleteDir()
+
+    try {
+        stage('Clone') {
+            Learn.Groovy()
+        }
+        stage('Build') {
+            sh "echo 'building ${config.projectName} ...'"
+        }
+        stage('Tests') {
+            parallel 'static': {
+                sh "echo 'shell scripts to run static tests...'"
+            },
+                    'unit': {
+                        sh "echo 'shell scripts to run unit tests...'"
+                    },
+                    'integration': {
+                        sh "echo 'shell scripts to run integration tests...'"
+                    }
+        }
+        stage('Deploy') {
+            sh "echo 'deploying to server ${config.serverDomain}...'"
+            sh "echo Itai ganot"
+            sh "echo Itai"
+        }
+    } catch (err) {
+        currentBuild.result = 'FAILED'
+        throw err
+    }
+}
