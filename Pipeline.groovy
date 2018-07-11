c522212187c5d65d3309ffffa4c2bac36e04407d
wuyou1102 <jotey@qq.com>
2018-07-11 14:03:52 +0800

    add

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -1,18 +1,26 @@
 node {
     // Clean workspace before doing anything
     deleteDir()
+    git url: 'http://192.168.90.181:8090/#/admin/projects/9201_1'
+
+    // Fetch the changeset to a local branch using the build parameters provided to the
+    // build by the Gerrit plugin...
+    def changeBranch = "change-${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}"
+    sh "git fetch origin ${GERRIT_REFSPEC}:${changeBranch}"
+    sh "git checkout ${changeBranch}"
     try {
         stage('Checkout') {
+
             echo "Checkout"
         }
         stage('Build') {
             echo "build"
         }
-        stage('Tests') {
-            echo "build"
+        stage('Test') {
+            echo "Test"
         }
         stage('Deploy') {
-            echo "build"
+            echo "Deploy"
         }
     } catch (err) {
         currentBuild.result = 'FAILED'
