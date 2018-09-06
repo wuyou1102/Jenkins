b0c9a7908b5bd34c3fe97c454760a0485e83847a
wuyou1102 <jotey@qq.com>
2018-09-06 16:03:20 +0800

    add

--- Checkout.py
+++ Checkout.py
@@ -2,7 +2,6 @@
 import os
 from libs import Utility
 
-
 # git
 # url: 'ssh://jenkins@192.168.90.181:29418/9201_1'
 #
@@ -19,10 +18,10 @@ from libs import Utility
 
 workspace_path = Utility.get_compiler_path()
 
-
 if not os.path.exists(workspace_path):
     print "Create New Folder: %s " % workspace_path
-    os.makedirs(workspace_path)
+    os.system()
+    os.makedirs(workspace_path, mode=755)
 os.chdir(workspace_path)
 stdin, stdout, stderr = os.popen3(
     Utility.Repo.init(url='ssh://youwu@gerrit.sensethink.cn:29418/manifest', branch='C2_8.1_master'))
