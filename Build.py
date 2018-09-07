ae98ebda37039ba0fdc11a0830bfbf87787ea63d
wuyou1102 <jotey@qq.com>
2018-09-07 13:57:02 +0800

    addd

--- Build.py
+++ Build.py
@@ -4,20 +4,20 @@ import os
 workspace_path = '/home/bspserver/sda/C2_SourceCode/c2_userdebug_20180906.180549/'
 os.chdir(workspace_path)
 
-envsetup_command = "/bin/bash build/envsetup.sh && build/link.sh && lunch g2-userdebug general && ulimit -c unlimited && make update-api -j8 && make -j8"
-link_command = ""
-lunch_command = "/bin/bash lunch g2-userdebug general"
-ulimit_command = "/bin/bash ulimit -c unlimited"
-update_command = "/bin/bash make update-api -j8"
-make_command = "/bin/bash make -j8"
+envsetup_command = "source build/envsetup.sh"
+link_command = "source build/link.sh"
+lunch_command = "lunch g2-userdebug general"
+ulimit_command = "ulimit -c unlimited"
+update_command = "make update-api -j8"
+make_command = "make -j8"
 
 commands = [
     envsetup_command,
-    # link_command,
-    # lunch_command,
-    # ulimit_command,
-    # update_command,
-    # make_command
+    link_command,
+    lunch_command,
+    ulimit_command,
+    update_command,
+    make_command
 ]
 
 for command in commands:
