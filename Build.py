e460ea25650ccdd6d99391651fbe5986c2488794
wuyou1102 <jotey@qq.com>
2018-09-07 14:37:54 +0800

    add

--- Build.py
+++ Build.py
@@ -4,17 +4,17 @@ import os
 workspace_path = '/home/bspserver/sda/C2_SourceCode/c2_userdebug_20180906.180549/'
 os.chdir(workspace_path)
 
-envsetup_command = "source build/envsetup.sh"
-link_command = "source build/link.sh"
-lunch_command = "lunch g2-userdebug general"
+envsetup_command = "source build/envsetup.sh && source build/link.sh && lunch g2-userdebug general"
+# link_command = ""
+# lunch_command = "lunch g2-userdebug general"
 ulimit_command = "ulimit -c unlimited"
 update_command = "make update-api -j8"
 make_command = "make -j8"
 
 commands = [
     envsetup_command,
-    link_command,
-    lunch_command,
+    # link_command,
+    # lunch_command,
     ulimit_command,
     update_command,
     make_command
