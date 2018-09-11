d8a40229c3e45903559be5db16ff85c9b2adb5a8
wuyou1102 <jotey@qq.com>
2018-09-11 14:29:32 +0800

    add deploy func

--- libs/Utility/Time.py
+++ libs/Utility/Time.py
@@ -1,6 +1,4 @@
 import time
-from libs import Environment as Env
-
 
 def get_timestamp(time_fmt='%Y_%m_%d-%H_%M_%S', t=None):
     t = t if t else time.time()
@@ -9,15 +7,3 @@ def get_timestamp(time_fmt='%Y_%m_%d-%H_%M_%S', t=None):
 
 def convert_timestamp(str, time_fmt='%Y_%m_%d-%H_%M_%S'):
     return time.mktime(time.strptime(str, time_fmt))
-
-
-def get_job_time():
-    start_time = Env.BUILD_TIMESTAMP
-    t = convert_timestamp(str=start_time, time_fmt="%Y%m%d.%H%M%S")
-    return t
-    t = t - 24 * 3 * 3600
-    return get_timestamp(time_fmt="%Y-%m-%d %H:%M", t=t)
-
-
-if __name__ == '__main__':
-    print get_job_start_time()
