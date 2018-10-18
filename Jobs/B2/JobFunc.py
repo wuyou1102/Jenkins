7c0f0bbc11e7a6b4e336ecbd1be60a9a085a5229
wuyou1102 <jotey@qq.com>
2018-10-18 20:28:01 +0800

    add

--- Jobs/B2/JobFunc.py
+++ Jobs/B2/JobFunc.py
@@ -1,5 +1,5 @@
 # -*- encoding:UTF-8 -*-
 
 
-PATH_SOURCE_CODE = ""
-PATH_DAILY_BUILD = ""
+PATH_SOURCE_CODE = "/home/bspserver/sda/B2_SourceCode"
+PATH_DAILY_BUILD = "/home/bspserver/sda/B2_DailyBuild"
