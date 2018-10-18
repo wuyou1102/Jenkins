f0f0c5f9b38c7135ae48b9dc616d6e1fe321aa04
wuyou1102 <jotey@qq.com>
2018-10-18 20:17:24 +0800

    add

--- Jobs/B2/__init__.py
+++ Jobs/B2/__init__.py
@@ -1,3 +1,11 @@
 # -*- encoding:UTF-8 -*-
-from Deploy import run as Deploy
+from Deploy import run as deploy
 from Build import run as build
+
+
+def checkout():
+    pass
+
+
+def test():
+    pass
