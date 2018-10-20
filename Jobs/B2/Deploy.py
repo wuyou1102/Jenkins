f830310da9beaefb7bcab43976a0e5d1d2585b4d
wuyou1102 <jotey@qq.com>
2018-10-20 14:57:07 +0800

    addd

--- Jobs/B2/Deploy.py
+++ Jobs/B2/Deploy.py
@@ -28,13 +28,14 @@ def deploy_p1():
 
 
 def copy_image(_type):
-    u_disk = 'artosyn-upgrade-B2%sU.' % _type
+    u_disk1 = 'artosyn-upgrade-B2%sU.' % _type
+    u_disk2 = 'B2%sU.' % _type
     full = 'B2%sF.' % _type
     dst_folder = Utility.create_folder(os.path.join(JobFunc.PATH_DAILY_BUILD, Env.BUILD_TIMESTAMP))
     for f in os.listdir(output_folder):
         if f.startswith(full):
             shutil.copy(src=os.path.join(output_folder, f), dst=os.path.join(dst_folder, "B2%sF.img" % _type))
-        elif f.startswith(u_disk):
+        elif f.startswith(u_disk1) or f.startswith(u_disk2):
             shutil.copy(src=os.path.join(output_folder, f), dst=os.path.join(dst_folder, "B2%sU.img" % _type))
         else:
             print "wuyou debug:->%s" % f
