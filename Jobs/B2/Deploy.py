ad07462eebd096b7884c942b2bd4084a49d53afc
wuyou1102 <jotey@qq.com>
2018-10-20 14:23:03 +0800

    add

--- Jobs/B2/Deploy.py
+++ Jobs/B2/Deploy.py
@@ -5,6 +5,36 @@ from libs import Utility
 import shutil
 import JobFunc
 
+output_folder = os.path.join(JobFunc.PATH_SOURCE_CODE, "9201", "output", "image")
+
 
 def run(*args, **kwargs):
-    pass
+    Utility.print_info(__file__, args, kwargs)
+    version_type = args[2]
+    if version_type == "P0":
+        deploy_p0()
+    elif version_type == "P1":
+        deploy_p1()
+    else:
+        JobFunc.RaiseException(KeyError, "Unknown version type:%s" % version_type)
+
+
+def deploy_p0():
+    copy_image("P0")
+
+
+def deploy_p1():
+    copy_image("P1")
+
+
+def copy_image(_type):
+    u_disk = 'artosyn-upgrade-B2%sU.' % _type
+    full = 'B2%sF.' % _type
+    dst_folder = Utility.create_folder(os.path.join(JobFunc.PATH_DAILY_BUILD, Env.BUILD_TIME, _type))
+    for f in os.listdir(output_folder):
+        if f.startswith(full):
+            shutil.copy(src=os.path.join(output_folder, f), dst=os.path.join(dst_folder, "B2%sF.img" % _type))
+        elif f.startswith(u_disk):
+            shutil.copy(src=os.path.join(output_folder, f), dst=os.path.join(dst_folder, "B2%sU.img" % _type))
+        else:
+            pass
