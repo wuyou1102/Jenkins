3eb4718af3e48eb0b9d197248d68f0fc7c49385f
wuyou1102 <jotey@qq.com>
2018-11-01 15:05:32 +0800

    add

--- Jobs/C2/Cleanup.py
+++ Jobs/C2/Cleanup.py
@@ -25,7 +25,7 @@ def remove_empty_commit_history():
 def remove_src_folder(folder_name):
     folder_path = os.path.join(SOURCE_CODE_PATH, folder_name)
     if os.path.exists(folder_path):
-        print folder_path
+        shutil.rmtree(folder_path)
 
 
 def remove_oldest_version():
@@ -34,7 +34,7 @@ def remove_oldest_version():
         print "free:%s" % free
         print "used:%s" % used
         print "total:%s" % total
-        if free < 950:
+        if free < 300:
             f_list = os.listdir(SOURCE_CODE_PATH)
             f_list.sort()
             oldest_folder = f_list[0]
