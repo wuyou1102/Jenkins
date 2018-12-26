17ddb71c8d9f6f049d1fe2fe441ea6a97cca3bc5
wuyou1102 <jotey@qq.com>
2018-12-26 16:48:01 +0800

    Update Incoming.py

--- libs/Utility/Worktile/Incoming.py
+++ libs/Utility/Worktile/Incoming.py
@@ -7,12 +7,15 @@ __incoming_url = 'https://hook.worktile.com/incoming/24eeed87286b43aea84ce87e437
 
 
 def job_started(users, build_info, text):
-    return Common.POST(__incoming_url, data=Message.job_started(build_info=build_info, text=text, users=users))
+    data = Message.job_started(build_info=build_info, text=text, users=users)
+    return Common.POST(__incoming_url, data=Common.JSON_DUMPS(data))
 
 
 def job_finished(users, build_info, text):
-    return Common.POST(__incoming_url, data=Message.job_finished(build_info=build_info, text=text, users=users))
+    data = Message.job_finished(build_info=build_info, text=text, users=users)
+    return Common.POST(__incoming_url, data=Common.JSON_DUMPS(data))
 
 
 def job_error(users, build_info, text):
-    return Common.POST(__incoming_url, data=Message.job_error(build_info=build_info, text=text, users=users))
+    data = Message.job_error(build_info=build_info, text=text, users=users)
+    return Common.POST(__incoming_url, data=Common.JSON_DUMPS(data))
